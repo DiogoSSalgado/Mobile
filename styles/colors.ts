6551ae1ae200b9d89610d2914f5f373e519572a7
Diogo <diogopequeno5@gmail.com>
2022-05-14 14:46:05 -0300

    Correções - 14/05

---
+++ styles/colors.ts
@@ -0,0 +1,13 @@
+
+
+
+export default {
+  black: '#000000',
+  white: '#ffffff',
+  primary: '#16181A',
+  primaryLight: '#CFD8DC',
+  secondary: '#459A30',
+  secondaryLight: '#B39DDB',
+  third: '#242E8A',
+  thirdLight: '#8C95E4'
+}
